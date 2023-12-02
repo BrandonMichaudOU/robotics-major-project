4ceea0b963729464299025b172d5f85ff0dee8e0
MajorDuck <annguyen3132@gmail.com>
2023-12-02 14:22:33 -0600

    Add performance metrics and simplify robot execution

--- robot.py
+++ robot.py
@@ -1,4 +1,4 @@
-
+from datetime import datetime
 
 class Robot:
     def __init__(self, graph, start_node, end_node):
@@ -8,33 +8,27 @@ class Robot:
         self.destination_node = end_node
         self.blocked_list = [] # maybe
 
-    def robot_go(self, algorithm, num_dynamic):
+    def robot_go(self, algorithm, proportion_dynamic, max_weight):
         # Performance metrics
         cost_travelled = 0
         num_iter = 0
+        start_time = datetime.now()
 
-        # generate initial path
+        # Generate initial path
         self.current_path, iterations = algorithm(self.graph, self.current_node, self.destination_node)
         num_iter += iterations
 
-        # create blocked list (or replace weights in map)
-        #   we use a blocked list in addition to editing
-        #   the weights directly 
-        # TODO: add num_dynamic-many nodes with increased weights
+        # Randomly add 'dynamic' obstacles to the map
+        self.graph.update_random_weight(proportion_dynamic, max_weight)
 
         # Travel through the path
-        while current_node != self.destination_node:
+        while self.current_node != self.destination_node:
             next_node = self.current_path[0]
-
-            # If next node is blocked, recalculate the path and retry
-            if next_node in self.blocked_list:
-                self.current_path, iterations = algorithm(self.graph, self.current_node, self.destination_node)
-                num_iter += iterations
-                continue
-
+            
             # Travel through the node
             cost_travelled += self.graph.get_weight(current_node, next_node)
             current_node = self.current_path.pop(0)
 
-            # Stretch goal: moving blocked nodes
-            # TODO
+        end_time = datetime.now()
+        runtime = end_time - start_time
+        return cost_travelled, num_iter, runtime
