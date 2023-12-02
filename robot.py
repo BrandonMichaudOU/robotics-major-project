0ba02e7052ea960eafb10f8a212d1de151a677d1
MajorDuck <annguyen3132@gmail.com>
2023-12-02 16:09:47 -0600

    Test and debug robot code

--- robot.py
+++ robot.py
@@ -1,8 +1,10 @@
 from datetime import datetime
+import random
+import graph
 
 class Robot:
-    def __init__(self, graph, start_node, end_node):
-        self.graph = graph
+    def __init__(self, g, start_node, end_node):
+        self.g = g
         self.current_path = []
         self.current_node = start_node
         self.destination_node = end_node
@@ -15,20 +17,203 @@ class Robot:
         start_time = datetime.now()
 
         # Generate initial path
-        self.current_path, iterations = algorithm(self.graph, self.current_node, self.destination_node)
+        self.current_path, seen, iterations = algorithm(self.g, self.current_node, self.destination_node)
         num_iter += iterations
 
         # Randomly add 'dynamic' obstacles to the map
-        self.graph.update_random_weight(proportion_dynamic, max_weight)
+        self.g.update_random_weights(proportion_dynamic, max_weight)
 
         # Travel through the path
+        self.current_path.pop(0) # remove starting node
         while self.current_node != self.destination_node:
             next_node = self.current_path[0]
             
             # Travel through the node
-            cost_travelled += self.graph.get_weight(current_node, next_node)
-            current_node = self.current_path.pop(0)
+            cost_travelled += self.g.get_weight(self.current_node, next_node)
+            self.current_node = self.current_path.pop(0)
 
         end_time = datetime.now()
         runtime = end_time - start_time
-        return cost_travelled, num_iter, runtime
+        return cost_travelled, num_iter, runtime.total_seconds() *1000
+
+
+def run_experiments(algorithm, num_runs = 100):
+    avg_costs = []
+    avg_iterations = []
+    avg_runtimes = []
+    
+
+    # Topological, small, static
+    size = 25
+    costs = []
+    iterations = []
+    runtimes = []
+
+    
+    for _ in range(num_runs):
+        g = graph.Graph()
+        g.random_topological_map(size, size*2, 2, 10, 10)
+        two_nodes = random.sample(g.get_nodes(), 2)
+        robot = Robot(g, two_nodes[0], two_nodes[1])
+        c, i, r = robot.robot_go(algorithm, 0, 100)
+        costs.append(c)
+        iterations.append(i)
+        runtimes.append(r)
+    avg_costs.append(sum(costs)/num_runs)
+    avg_iterations.append(sum(iterations)/num_runs)
+    avg_runtimes.append(sum(runtimes)/num_runs)
+
+    # Topological, small, dynamic
+    size = 25
+    costs = []
+    iterations = []
+    runtimes = []
+
+    
+    for _ in range(num_runs):
+        g = graph.Graph()
+        g.random_topological_map(size, size*2, 2, 10, 10)
+        two_nodes = random.sample(g.get_nodes(), 2)
+        robot = Robot(g, two_nodes[0], two_nodes[1])
+        c, i, r = robot.robot_go(algorithm, .10, 100)
+        costs.append(c)
+        iterations.append(i)
+        runtimes.append(r)
+    avg_costs.append(sum(costs)/num_runs)
+    avg_iterations.append(sum(iterations)/num_runs)
+    avg_runtimes.append(sum(runtimes)/num_runs)
+
+    # Topological, big, static
+    size = 100
+    costs = []
+    iterations = []
+    runtimes = []
+
+    
+    for _ in range(num_runs):
+        g = graph.Graph()
+        g.random_topological_map(size, size*2, 2, 10, 10)
+        two_nodes = random.sample(g.get_nodes(), 2)
+        robot = Robot(g, two_nodes[0], two_nodes[1])
+        c, i, r = robot.robot_go(algorithm, 0, 100)
+        costs.append(c)
+        iterations.append(i)
+        runtimes.append(r)
+    avg_costs.append(sum(costs)/num_runs)
+    avg_iterations.append(sum(iterations)/num_runs)
+    avg_runtimes.append(sum(runtimes)/num_runs)
+
+    # Topological, big, dynamic
+    size = 100
+    costs = []
+    iterations = []
+    runtimes = []
+
+    
+    for _ in range(num_runs):
+        g = graph.Graph()
+        g.random_topological_map(size, size*2, 2, 10, 10)
+        two_nodes = random.sample(g.get_nodes(), 2)
+        robot = Robot(g, two_nodes[0], two_nodes[1])
+        c, i, r = robot.robot_go(algorithm, .10, 100)
+        costs.append(c)
+        iterations.append(i)
+        runtimes.append(r)
+    avg_costs.append(sum(costs)/num_runs)
+    avg_iterations.append(sum(iterations)/num_runs)
+    avg_runtimes.append(sum(runtimes)/num_runs)
+
+
+    # Metric, small, static
+    num_cols = 10
+    num_rows = 10
+    costs = []
+    iterations = []
+    runtimes = []
+
+    
+    for _ in range(num_runs):
+        g = graph.Graph()
+        g.random_metric_map(num_rows, num_cols, 2)
+        two_nodes = random.sample(g.get_nodes(), 2)
+        robot = Robot(g, two_nodes[0], two_nodes[1])
+        c, i, r = robot.robot_go(algorithm, 0, 100)
+        costs.append(c)
+        iterations.append(i)
+        runtimes.append(r)
+    avg_costs.append(sum(costs)/num_runs)
+    avg_iterations.append(sum(iterations)/num_runs)
+    avg_runtimes.append(sum(runtimes)/num_runs)
+
+    # Metric, small, dynamic
+    num_cols = 10
+    num_rows = 10
+    costs = []
+    iterations = []
+    runtimes = []
+
+    
+    for _ in range(num_runs):
+        g = graph.Graph()
+        g.random_metric_map(num_rows, num_cols, 2)
+        two_nodes = random.sample(g.get_nodes(), 2)
+        robot = Robot(g, two_nodes[0], two_nodes[1])
+        c, i, r = robot.robot_go(algorithm, .10, 100)
+        costs.append(c)
+        iterations.append(i)
+        runtimes.append(r)
+    avg_costs.append(sum(costs)/num_runs)
+    avg_iterations.append(sum(iterations)/num_runs)
+    avg_runtimes.append(sum(runtimes)/num_runs)
+
+    # Metric, big, static
+    num_cols = 20
+    num_rows = 20
+    costs = []
+    iterations = []
+    runtimes = []
+
+    
+    for _ in range(num_runs):
+        g = graph.Graph()
+        g.random_metric_map(num_rows, num_cols, 2)
+        two_nodes = random.sample(g.get_nodes(), 2)
+        robot = Robot(g, two_nodes[0], two_nodes[1])
+        c, i, r = robot.robot_go(algorithm, 0, 100)
+        costs.append(c)
+        iterations.append(i)
+        runtimes.append(r)
+    avg_costs.append(sum(costs)/num_runs)
+    avg_iterations.append(sum(iterations)/num_runs)
+    avg_runtimes.append(sum(runtimes)/num_runs)
+
+    # Metric, big, dynamic
+    num_cols = 20
+    num_rows = 20
+    costs = []
+    iterations = []
+    runtimes = []
+
+    
+    for _ in range(num_runs):
+        g = graph.Graph()
+        g.random_metric_map(num_rows, num_cols, 10)
+        two_nodes = random.sample(g.get_nodes(), 2)
+        robot = Robot(g, two_nodes[0], two_nodes[1])
+        c, i, r = robot.robot_go(algorithm, .10, 100)
+        costs.append(c)
+        iterations.append(i)
+        runtimes.append(r)
+    avg_costs.append(sum(costs)/num_runs)
+    avg_iterations.append(sum(iterations)/num_runs)
+    avg_runtimes.append(sum(runtimes)/num_runs)
+
+    return avg_costs, avg_iterations, avg_runtimes
+
+
+if __name__ == "__main__":
+    print("Breadth-first search experiments")
+    c, i, r = run_experiments(graph.breadth_first_search, 200)
+    print(c)
+    print(i)
+    print(r)
